4b33434d822f0dbd33b812341b5c7c0d1193be39
FranciscoMarini <marinifrancisco48@gmail.com>
2020-05-10 19:01:11 -0300

    menu changes

--- main.c
+++ main.c
@@ -4,45 +4,143 @@
 
 int pasarPilaDistinta (Pila * pOrigen, Pila * pDestino, int elemento);
 void cargarPila(Pila * punteroPila);
-void CuadradoArreglo (int Z[], int cantVal);
-void ArregloInverso (int Z[], int cantVal);
-void SumaArreglo (int Z[], int cantVal);
-void ArregloDePila (Pila * A, Pila * B, int Z[], int cantVal);
-void MostrarArreglo (int Z[], int cantVal);
-void pasarpila (Pila * A, Pila * B);
+void cuadradoArreglo (int Z[], int cantVal);
+void arregloInverso (int Z[], int cantVal);
+void sumaArreglo (int Z[], int cantVal);
+void arregloDePila (Pila * A, Pila * B, int Z[], int cantVal);
+void mostrarArreglo (int Z[], int cantVal);
+void pasarPila (Pila * A, Pila * B);
 void agregaEnBase (Pila * punteroPila, int elemento);
+void eliminaBasePila (Pila * A);
 
 int main()
 {
-    /*///Ejercicio 1.
-    Pila A;
-    Pila B;
-    inicpila(&A);
-    inicpila(&B);
+    int ejercicio;
+    char control;
 
-    cargarPila(&A);
-    cargarPila(&B);
-    mostrar(&A);
-    mostrar(&B);
+    do
+    {
 
-    int elementos = pasarPilaDistinta(&A,&B,5);
+        system("cls");
+        printf("Escriba el numero del ejercicio al que quiere ir:\n");
+        printf("Ejercicio 1: Pasar los elementos de la pila A a la B que sean distintos al numero ingresado\n");
+        printf("Ejercicio 2: Hacer el cuadrado de los elementos del arreglo Z y lo almacene en otro arreglo.\n");
+        printf("Ejercicio 3: Dado un arreglo de numeros enteros, genere otro en orden inverso.\n");
+        printf("Ejercicio 4: Realizar la suma del contenido de todo los elementos anteriores al indice de un arreglo\n");
+        printf("Ejercicio 5: Hacer una funcion que pase los elementos de ambas pilas al arreglo, manteniendo el orden\n");
+        printf("Ejercicio 6: Hacer una funcion que agregue un elemento a la base de una pila\n");
+        printf("Ejercicio 7: Eliminar el elemento que se encuentra en la base de una pila.\n");
 
-    mostrar(&A);
-    mostrar(&B);
-    printf("se pasaron %d elementos", elementos);*/
-    ///Ejercicio 2.
-    /*int N[];{1,2,3,4,5};
+        scanf("%i", &ejercicio);
 
-    CuadradoArreglo (N[], int 5);*/
+        switch (ejercicio)
+        {
+        case 1:
 
-    ///Ejercicio 3.
-   /*int N[]= {1,2,3,4,5};
+           {
+                    Pila A;
+                    Pila B;
+                    inicpila(&A);
+                    inicpila(&B);
 
-   ArregloInverso(N[], 5);*/
+                    apilar(&A,4);
+                    apilar(&A,5);
+                    apilar(&A,6);
 
-   int N[]={1,5,6,7,8};
-   SumaArreglo(N[],5);
+                    apilar(&B,4);
+                    apilar(&B,5);
+                    apilar(&B,6);
+                    int elemento;
 
+                    mostrar(&A);
+                    mostrar(&B);
+
+                    elemento=pasarPilaDistinta(&A,&B,5);
+                    printf("cantidad de veces:\n%d",elemento);
+
+                    mostrar(&A);
+                    mostrar(&B);
+           }
+                break;
+
+            case 2:
+            {
+                int N[3]={2,3,4};
+                cuadradoArreglo(N,3);
+            }
+
+
+
+                break;
+
+            case 3:
+                {
+                    int N[3]={2,3,4};
+                        arregloInverso(N,3);
+
+                }
+                break;
+
+            case 4:
+                {
+                    int N[]={1,5,6,7,8};
+
+                    sumaArreglo(N,5);
+                }
+
+                break;
+
+            case 5:
+
+                    {
+
+                        Pila A;
+                        Pila B;
+                        inicpila(&A);
+                        inicpila(&B);
+                        int N[6]={0};
+
+                        arregloDePila(&A,&B,N,6);
+                    }
+
+                break;
+
+            case 6:
+                {
+                    Pila A;
+                    inicpila(&A);
+                    apilar(&A,4);
+                    apilar(&A,5);
+                    apilar(&A,6);
+
+                    agregaEnBase(&A,5);
+
+                }
+                break;
+
+            case 7:
+                {
+                   Pila A;
+                    inicpila(&A);
+                    apilar(&A,4);
+                    apilar(&A,5);
+                    apilar(&A,6);
+
+                    eliminaBasePila(&A);
+                }
+                break;
+
+                default:
+
+                printf("Ese ejercicio no existe \n");
+
+            }
+             printf("Quiere ver otro ejercicio? s/n\n");
+            fflush(stdin);
+            printf("%c\n", control = getch());
+
+    }
+    while(control == 's');
 
 
     return 0;
@@ -92,62 +190,63 @@ void cargarPila(Pila * punteroPila)
 
 ///(2)Hacer una función que calcule el cuadrado de los
 ///    elementos del arreglo Z y lo almacene en otro arreglo.
-void CuadradoArreglo (int Z[], int cantVal)
+void cuadradoArreglo (int Z[], int cantVal)
 {
     int i = 0;
+    int N[cantVal];
 
-
-    for (i=0; i<cantVal, i++)
+    for (i=0; i<cantVal; i++)
     {
-        Z[i]= i*i;
-        printf("el cuadrado es... \n &d", Z[i]);
-    }
+        N[i]= Z[i]*Z[i];
 
+    }
+   printf("el arreglo nuevo es: ");
+   mostrarArreglo(N,cantVal);
 }
-///(3)Escriba un programa que dado un arreglo...........................
+///(3)Escriba un programa que dado un arreglo
 ///   de números enteros, genere otro en orden inverso.
 
-void ArregloInverso (int Z[], int cantVal)
+void arregloInverso (int Z[], int cantVal)
 {
-      int i =0;
+      int i,j=0;
+      int Y[]={};
 
-      for (i=cantVal-1; i>=0;i--)
+
+      for (i=cantVal-1; i<=0; i--)
       {
-          printf("%d",Z[i]);
+          Y[j]=Z[i];
+          j++;
       }
-
+     printf("el nuevo arreglo es: \n");
+     mostrarArreglo(Y, cantVal);
 }
 ///(4){1,5,6,7,8} escribir un programa que genere otro arreglo con la suma del..............................
 ///contenido de todo los elementos anteriores al índice actual. El resultado final
 ///del segundo arreglo es el siguiente: {1,6,12,19,27}.
-void SumaArreglo (int Z[], int cantVal)
+void sumaArreglo (int Z[], int cantVal)
 {
-    int i,j;
+    int i;
+    printf("el arreglo es:\n");
+    mostrarArreglo(Z, cantVal);
 
-    for (i=0;i<cantVal; i++)
+    for(i=0;i<cantVal; i++)
     {
         if (i!=0)
         {
             Z[i]+=Z[i-1];
         }
     }
-    for (j=0; j<cantVal; j++)
-    {
-        printf("nuevo arreglo: %d\n",Z[j]);
-
-    }
+    printf("\nel nuevo arreglo es:\n");
+    mostrarArreglo(Z, cantVal);
 }
 
 ///(5)Dadas dos pilas ordenadas......................................................
 ///y un arreglo vacío, hacer una función que pase los
 ///elementos de ambas pilas al arreglo, manteniendo el orden.
-void ArregloDePila (Pila * A, Pila * B, int Z[], int cantVal)
+void arregloDePila (Pila * A, Pila * B, int Z[], int cantVal)
 {
     Pila aux;
     inicpila(&aux);
-
-    cargarPila(&A);
-    cargarPila (&B);
 
     int i=0;
 
@@ -155,7 +254,7 @@ void ArregloDePila (Pila * A, Pila * B, int Z[], int cantVal)
     {
          for (i=0; i<cantVal; i++)
          {
-             Z[i] == desapilar(&A);
+             Z[i] = desapilar(&A);
          }
 
 
@@ -164,7 +263,7 @@ void ArregloDePila (Pila * A, Pila * B, int Z[], int cantVal)
     {
          for (i=0; i<cantVal; i++)
          {
-             Z[i] == desapilar(&B);
+             Z[i] = desapilar(&B);
          }
 
 
@@ -173,14 +272,13 @@ void ArregloDePila (Pila * A, Pila * B, int Z[], int cantVal)
 
 }
 ///Mostrar los elementos de un arreglo
-void MostrarArreglo (int Z[], int cantVal)
+void mostrarArreglo (int Z[], int cantVal)
 {
-     int i = 0;
+    int i = 0;
 
-
-    for (i=0; i<cantVal, i++)
+    for (i=0; i<cantVal; i++)
     {
-        printf("&d", Z[i]);
+        printf("\n%d", Z[i]);
     }
 }
 
@@ -191,22 +289,33 @@ void agregaEnBase (Pila * punteroPila, int elemento)
     Pila aux;
     inicpila(&aux);
 
-    while(!pilavacia(punteroPila))
+    printf("Pila original");
+    mostrar(punteroPila);
+
+    while (!pilavacia(punteroPila))
     {
-        pasarpila(punteroPila, &aux);
-        apilar(punteroPila, elemento);
-        pasarpila(&aux, punteroPila);
-        mostrar(punteroPila);
+        pasarPila(punteroPila, &aux);
+        printf("Pila Aux");
+        mostrar(&aux);
     }
+
+    apilar(punteroPila, elemento);
+
+    while(!pilavacia(&aux))
+    {
+        pasarPila(&aux, punteroPila);
+    }
+
+    printf("la pila nueva es:");
+    mostrar(punteroPila);
 
 }
 ///pasa elementos de la pila A a la pila B
-void pasarpila (Pila * A, Pila * B)
+void pasarPila (Pila * A, Pila * B)
 {
     while (!pilavacia(A))
     {
         apilar(B, desapilar(A));
-        mostrar(B);
 
     }
 
@@ -216,17 +325,37 @@ void pasarpila (Pila * A, Pila * B)
 void eliminaBasePila (Pila * A)
 {
     Pila aux;
+    Pila Basura;
     inicpila(&aux);
+    inicpila(&Basura);
 
-    pasarpila(A, &aux);
-    desapilar(tope(&aux));
-    pasarpila(&aux, A);
+
+    printf("Pila Origen");
+    mostrar(A);
+    while (!pilavacia(A))
+    {
+        pasarPila(A, &aux);
+
+    }
+        apilar(&Basura, desapilar(&aux));
+        pasarPila(&aux, A);
+
+        printf("Pila nueva:");
+        mostrar(A);
+
 }
-
 
 ///   -* &'s
 ///   -* punteros
 ///   -* ejercicios
+
+
+
+
+
+
+
+
 
 
 
